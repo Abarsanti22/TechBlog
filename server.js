1caa431d13a80ca2a47a5ce1dfc38018c0d9cb23
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-11-19 14:46:36 -0500

    updated server

--- server.js
+++ server.js
@@ -3,3 +3,38 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const session = require('express-session');
 const exphbs = require('express-handlebars')
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({helpers});
+
+const sess = {
+    secret: process.env.DB_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore ({
+        db: sequelize,
+        checkExpirationInterval: 1000 * 60 * 10,
+        expiration: 1000 * 60 * 30
+    })
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(session(sess))
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(routes);
+
+sequelize.sync();
+
+app.listen(PORT, () => {
+    console.log('Now listening');
+});
