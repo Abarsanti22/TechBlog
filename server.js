cc71ccb6eaa1907212a0c02389cdc7efc627144f
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-11-19 08:04:33 -0500

    added paths

---
+++ server.js
@@ -0,0 +1,5 @@
+const path = require('path');
+const express = require('express');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const exphbs = require('express-handlebars')
